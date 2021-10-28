5b1cd306f0780df676a81632c421f110fbeaa53d
cjettie <ferundal@gmail.com>
2021-10-28 12:54:34 +0300

    more test for ex03

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -2,13 +2,56 @@
 #include "Point.hpp"
 
 int main( void ) {
-	Point triangle[3] = {
-		Point (0.0f, 0.0f),
-		Point (0.0f, 1.0f),
-		Point (1.0f, 0.0f)
-	};
-	Point outside_point(1.0f, 1.0f);
-	Point inside_point(0.2f, 0.2f);
-	std::cout << bsp (triangle[0], triangle[1], triangle[2], outside_point) << std::endl;
-	std::cout << bsp (triangle[0], triangle[1], triangle[2], inside_point) << std::endl;
+	{
+		Point triangle[3] = {
+				Point(0.0f, 0.0f),
+				Point(0.0f, 1.0f),
+				Point(1.0f, 0.0f)
+		};
+		Point outside_point(1.0f, 1.0f);
+		Point inside_point(0.2f, 0.2f);
+		std::cout << bsp(triangle[0], triangle[1], triangle[2],
+						 outside_point) << std::endl;
+		std::cout << bsp(triangle[0], triangle[1], triangle[2],
+						 inside_point) << std::endl;
+	}
+	{
+		Point triangle[3] = {
+				Point(0.0f, 0.0f),
+				Point(0.0f, -1.0f),
+				Point(1.0f, 0.0f)
+		};
+		Point outside_point(1.0f, -1.0f);
+		Point inside_point(0.2f, -0.2f);
+		std::cout << bsp(triangle[0], triangle[1], triangle[2],
+						 outside_point) << std::endl;
+		std::cout << bsp(triangle[0], triangle[1], triangle[2],
+						 inside_point) << std::endl;
+	}
+	{
+		Point triangle[3] = {
+				Point(0.0f, 0.0f),
+				Point(0.0f, 1.0f),
+				Point(-1.0f, 0.0f)
+		};
+		Point outside_point(-1.0f, 1.0f);
+		Point inside_point(-0.2f, 0.2f);
+		std::cout << bsp(triangle[0], triangle[1], triangle[2],
+						 outside_point) << std::endl;
+		std::cout << bsp(triangle[0], triangle[1], triangle[2],
+						 inside_point) << std::endl;
+	}
+	{
+		Point triangle[3] = {
+				Point(0.0f, 0.0f),
+				Point(0.0f, -1.0f),
+				Point(-1.0f, 0.0f)
+		};
+		Point outside_point(-1.0f, -1.0f);
+		Point inside_point(-0.2f, -0.2f);
+		std::cout << bsp(triangle[0], triangle[1], triangle[2],
+						 outside_point) << std::endl;
+		std::cout << bsp(triangle[0], triangle[1], triangle[2],
+						 inside_point) << std::endl;
+	}
 }
